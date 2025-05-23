ec5f7e0c2334c2c4b288b765e2d929847575179a
tkjftkjf <qkrwlsdn1234321@gmail.com>
2025-05-23 13:12:10 +0900

    feat: update tag widget to support various values

--- lib/widgets/tag_widget.dart
+++ lib/widgets/tag_widget.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-enum TagType { boolean, text, fixed }
+enum TagType { boolean, text, time }
 
 class TagWidget extends StatelessWidget {
   const TagWidget({
@@ -15,6 +15,33 @@ class TagWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    String tagValueOutput;
+    List<String> timeValueOutput = [
+      "0시~2시",
+      "2시~4시",
+      "4시~6시",
+      "6시~8시",
+      "8시~10시",
+      "10시~12시",
+      "12시~14시",
+      "14시~16시",
+      "16시~18시",
+      "18시~20시",
+      "20시~22시",
+      "22시~24시",
+    ];
+    switch (tagType) {
+      case TagType.boolean:
+        tagValueOutput = tagValue ? "O" : "X";
+        break;
+      case TagType.text:
+        tagValueOutput = tagValue;
+        break;
+      case TagType.time:
+        tagValueOutput = timeValueOutput[tagValue];
+        break;
+    }
+
     return Container(
       decoration: BoxDecoration(
         color: Colors.blue.shade100,
@@ -36,7 +63,7 @@ class TagWidget extends StatelessWidget {
           ),
           SizedBox(width: 5),
           Text(
-            (tagValue ? "O" : "X"),
+            tagValueOutput,
             style: TextStyle(fontSize: 10, fontWeight: FontWeight.w500),
           ),
         ],
