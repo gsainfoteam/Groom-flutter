e4de7f717b87250974a786e35ef6613205f56579
tkjftkjf <qkrwlsdn1234321@gmail.com>
2025-05-24 23:35:40 +0900

    feat: add ChattingListPage

---
+++ lib/pages/chatting_list_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:groom_flutter/widgets/chat_tile_widget.dart';
+
+class ChattingListPage extends StatefulWidget {
+  const ChattingListPage({super.key});
+
+  @override
+  State<ChattingListPage> createState() => _ChattingListPageState();
+}
+
+class _ChattingListPageState extends State<ChattingListPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.fromLTRB(35, 30, 35, 10),
+      width: double.infinity,
+      child: ListView(
+        children: [ChatTileWidget(), ChatTileWidget(), ChatTileWidget()],
+      ),
+    );
+  }
+}
