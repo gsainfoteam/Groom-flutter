5c1feffc7303b31a78136afcd8a59e5f633a3d5a
tkjftkjf <qkrwlsdn1234321@gmail.com>
2025-05-24 13:28:35 +0900

    fix: update secondary color in theme and add bodySmall text style

--- lib/constants/theme.dart
+++ lib/constants/theme.dart
@@ -4,9 +4,10 @@ final defaultTheme = ThemeData(
   colorScheme: ColorScheme.fromSeed(
     seedColor: Colors.white,
     primary: Color(0xFFFFC2C2),
-    secondary: Color(0xFFE7E7E7),
+    secondary: Color(0xFFDBDBDB),
   ),
   textTheme: TextTheme(
+    bodySmall: TextStyle(fontSize: 11, fontWeight: FontWeight.w600),
     bodyMedium: TextStyle(fontSize: 16, fontWeight: FontWeight.w400),
     bodyLarge: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
     titleLarge: TextStyle(fontSize: 25, fontWeight: FontWeight.w600),
